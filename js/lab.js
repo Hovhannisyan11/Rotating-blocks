43ea9a14ddbfd0d99a0dbf17801cfa70497b9b60
Hovhannisyan11 <garik.hovhannisyan.11@mail.ru>
2023-06-27 19:30:30 +0400

    first commit

---
+++ js/lab.js
@@ -0,0 +1,88 @@
+d = document.querySelector('div');
+d = document.querySelectorAll('div');
+//console.log(d);
+
+for(let i = 0;i<d.length;i++){
+    d[i].addEventListener('click',f);
+
+}
+
+function f(){
+
+for(let i = 0;i<d.length;i++){
+  d[i].style.background = 'transparent';
+  d[i].style.transform = 'rotate (90deg)';
+  d[i].style.borderRadius = '0';
+}
+
+let c = parseInt(Math.random() * 99999);
+this.style.background = '#' + c;
+this.style.borderRadius = '20px'; // this-y vrody c-i heta
+this.style.transition = '3s';
+this.style.transform = 'rotate(45deg)';
+
+
+
+}
+
+
+function f1(){
+ let c = parseInt(Math.random() * 99999999)
+ for(let i = 0;i<d.length;i++){
+  d[i].style.background = '#' + c;
+  d[i].style.borderRadius = '50%';
+  d[i].style.transition = '2s';
+  d[i].style.transform = 'scale(1.5)';
+ }
+
+
+}
+
+let x = 25;
+
+function f3(){
+ let c = parseInt(Math.random() * 99999999);
+ for(let i = 0;i<d.length;i++){
+    d[i].style.background = '#' + c;
+    d[i].style.background = "rotate("   +   x   +  "deg)";
+    x+=25;
+    d[i].style.transition = '1s';
+    d[i].style.borderRadius = '0';
+ };
+};
+
+
+
+function f5(){
+  let c = parseInt(Math.random() * 9999999);
+  for(let i = 0;i<d.length;i++){
+    d[i].style.background = '#' + c; // esi en guyneri pahna #02153 orinak
+    d[i].style.transform = 'rotate(' + x + 'deg)';
+    d[i].style.transition = '1s';
+    d[i].style.borderRadius = '0';
+
+
+  }
+
+  x+=25
+}
+
+
+function f6(){
+   for(let i = 0;i<d.length;i++){
+    let t =+ d[i].getAttribute('data-tox'); // esi vercnuma data-tox-i '' -i  meji tivy
+    
+    
+    let s =+ d[i].getAttribute('data-syun'); // esele vercnuma data-syun-i ''-i meji tivy
+    if(t == s || (t+s) == 3){
+      d[i].style.background = '#fec00055';
+      d[i].style.transform = 'scale(1.5';
+      d[i].style.borderRadius = '50px'
+    }else {
+      d[i].style.background = '#000';
+    }
+   }
+
+
+}
+
