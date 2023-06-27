9b188f20d83af79dcbf833e15f2d1e97e847a6bb
Hovhannisyan11 <garik.hovhannisyan.11@mail.ru>
2023-06-27 20:20:09 +0400

    first commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 d = document.querySelector('div');
 d = document.querySelectorAll('div');
-//console.log(d);
+
 
 for(let i = 0;i<d.length;i++){
     d[i].addEventListener('click',f);
@@ -17,7 +17,7 @@ for(let i = 0;i<d.length;i++){
 
 let c = parseInt(Math.random() * 99999);
 this.style.background = '#' + c;
-this.style.borderRadius = '20px'; // this-y vrody c-i heta
+this.style.borderRadius = '20px'; 
 this.style.transition = '3s';
 this.style.transform = 'rotate(45deg)';
 
@@ -56,7 +56,7 @@ function f3(){
 function f5(){
   let c = parseInt(Math.random() * 9999999);
   for(let i = 0;i<d.length;i++){
-    d[i].style.background = '#' + c; // esi en guyneri pahna #02153 orinak
+    d[i].style.background = '#' + c; 
     d[i].style.transform = 'rotate(' + x + 'deg)';
     d[i].style.transition = '1s';
     d[i].style.borderRadius = '0';
@@ -70,10 +70,10 @@ function f5(){
 
 function f6(){
    for(let i = 0;i<d.length;i++){
-    let t =+ d[i].getAttribute('data-tox'); // esi vercnuma data-tox-i '' -i  meji tivy
+    let t =+ d[i].getAttribute('data-tox'); 
     
     
-    let s =+ d[i].getAttribute('data-syun'); // esele vercnuma data-syun-i ''-i meji tivy
+    let s =+ d[i].getAttribute('data-syun'); 
     if(t == s || (t+s) == 3){
       d[i].style.background = '#fec00055';
       d[i].style.transform = 'scale(1.5';
